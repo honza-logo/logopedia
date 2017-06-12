ef01032a02a703f7f9d0deff38b598c2f11e5981
honza <honza@logopedia.cz>
2017-06-12 16:52:28 +0200

    learning part skeleton fnalized, added navbar

--- slovnik/utils.py
+++ slovnik/utils.py
@@ -1,18 +1,22 @@
 from slovnik.models import Word
 
 
-def get_surrounding_words_in_learning(category, word):
+def get_word_details(category, word):
     words_ordered = Word.objects.filter(category__category_short_name=category).order_by('word_short_name')
-    prev = ''
-    next = ''
+    total    = words_ordered.count()
+    prev_item = ''
+    next_item = ''
     hit = False
+    current_position = 1
     for index, item in enumerate(words_ordered):
         if hit:
-            next = item
+            next_item = item
             break
         if item.word_short_name == word:
             hit = True
         else:
-            prev = item
+            prev_item = item
+            current_position += 1
 
-    return {'prev': prev, 'next': next}
+    return {'prev': prev_item, 'next': next_item, 'word_count': total, 'current_position': current_position}
+
--- slovnik/views.py
+++ slovnik/views.py
@@ -31,7 +31,6 @@ class DetailView(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['category'] = self.kwargs['cat']
-        context['word_links'] = utils.get_surrounding_words_in_learning(self.kwargs['cat'], self.kwargs['word'])
+        context['word_details'] = utils.get_word_details(self.kwargs['cat'], self.kwargs['word'])
         return context
 
